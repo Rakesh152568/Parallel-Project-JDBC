6e22dcfa4c9bb03e7b4ae0d5f8d88eb9a3fd4478
bababu <bababu@BLRTRNGGF02041.corp.capgemini.com>
2018-08-06 16:49:11 +0530

    version3

---
+++ JDBCParallelProject/src/com/cg/payment/Exception/PaymentException.java
@@ -0,0 +1,12 @@
+package com.cg.payment.Exception;
+
+public class PaymentException extends Exception {
+	public PaymentException(){
+		super();
+		
+	}
+
+	public PaymentException(String message){
+		super(message);
+	}
+}
